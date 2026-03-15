4b8d082bc3c20df260287fbe0ad2ee9b0272d5d5
BHANUTEJA230 <singamanenibhanu2@gmail.com>
2026-03-15 16:02:47 +0530

    Create LibraryManagementSystem.java

---
+++ LibraryManagementSystem.java
@@ -0,0 +1,8 @@
+package com.library.main;
+
+public class LibraryManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Library Management System using Java and MySQL");
+    }
+}
